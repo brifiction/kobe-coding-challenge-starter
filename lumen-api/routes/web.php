1273b9eeb4a32501d2bc82d799a403d01fd6aa68
brian ng <brianngchongeu@gmail.com>
2020-02-10 16:52:07 +1100

    finalized web routes for user

--- lumen-api/routes/web.php
+++ lumen-api/routes/web.php
@@ -11,6 +11,33 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
-});
+//$router->get('/', function () use ($router) {
+//    return $router->app->version();
+//});
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->group(['prefix' => 'api/'], function () use ($router) {
+
+    $router->get('/', function () {
+        $response['success'] = true;
+        $response['result'] = "Welcome to Brian's Tabletop Games (Lumen API)";
+        return response($response);
+    });
+
+    $router->post('login', 'LoginController@login');
+    $router->post('register', 'UserController@register');
+
+    $router->get('user/{id}', ['middleware' => 'auth', 'uses' => 'UserController@getUser']);
+    $router->get('user/api/{api_token}', 'UserController@getUserApi');
+}
+);
