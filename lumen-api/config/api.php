7ed31ee845031a7e3861b89fb8224d86b2fefb1b
brian ng <brianngchongeu@gmail.com>
2020-02-11 15:50:39 +1100

    define api endpoint prefixes

--- lumen-api/config/api.php
+++ lumen-api/config/api.php
@@ -1,20 +1,6 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | API URL
-    |--------------------------------------------------------------------------
-    |
-    | This URL is used by the console to properly generate URLs when using
-    | the Artisan command line tool. You should set this to the root of
-    | your application so that it is used when running Artisan tasks.
-    |
-    */
-
-    'url' => env('APP_URL', 'http://localhost:8000'),
-
     /*
     |--------------------------------------------------------------------------
     | API Endpoints
@@ -25,11 +11,9 @@ return [
     */
 
     'endpoint' => [
-
-        'product' => '/api/product/',
-        'user' => '/api/user/',
-        'cart' => '/api/cart/',
-        'coupon' => '/api/coupon/',
+        'product' => env('APP_URL', 'http://localhost:8000').'/api/product/',
+        'user' => env('APP_URL', 'http://localhost:8000').'/api/user/',
+        'cart' => env('APP_URL', 'http://localhost:8000').'/api/cart/',
+        'coupon' => env('APP_URL', 'http://localhost:8000').'/api/coupon/',
     ],
-
 ];
