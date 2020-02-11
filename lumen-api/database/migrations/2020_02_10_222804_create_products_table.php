a1723a2fc5ecf78d977b143e225965b4522ea7d6
brian ng <brianngchongeu@gmail.com>
2020-02-11 12:13:35 +1100

    updated product description field type

--- lumen-api/database/migrations/2020_02_10_222804_create_products_table.php
+++ lumen-api/database/migrations/2020_02_10_222804_create_products_table.php
@@ -16,7 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('description');
+            $table->text('description');
             $table->integer('inventory');
             $table->float('price');
             $table->boolean('sold')->default('0');
